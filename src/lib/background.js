067e8a5bf622003f2aa20f09c645f5b9a3e037c4
Tim Jones <jonestimd@gmail.com>
2019-04-16 20:37:28 -0500

    Enable page action when URL matches a saved password

--- src/lib/background.js
+++ src/lib/background.js
@@ -5,21 +5,41 @@
 'use strict';
 import * as settings from './settings';
 
+const cssMatchers = ['input[type="password"]', 'input[type="text"][id*="user" i]'];
+
+function newRule(pageUrl) {
+    return {pageUrl, css: cssMatchers};
+}
+
+function getUrlRule(urlPaths) {
+    const conditions = Object.keys(urlPaths).map(urlString => {
+        try {
+            const url = new URL(urlString);
+            const pageUrl = { hostEquals: url.hostname, schemes: [url.protocol.replace(':', '')] };
+            if (url.port) pageUrl.ports = [parseInt(url.port)];
+            if (url.pathname.length > 1) pageUrl.pathPrefix = url.pathname;
+            if (url.search) pageUrl.queryContains = url.search.substr(1);
+            return new chrome.declarativeContent.PageStateMatcher(newRule(pageUrl));
+        } catch (err) { // just the hostname
+            return new chrome.declarativeContent.PageStateMatcher(newRule({ hostEquals: urlString, schemes: ['https'] }));
+        }
+    });
+    return { conditions, actions: [new chrome.declarativeContent.ShowPageAction()] };
+}
+
+function setPageRules(urlPaths) {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+        if (urlPaths) chrome.declarativeContent.onPageChanged.addRules([getUrlRule(urlPaths)]);
+    });
+}
+
 chrome.runtime.onInstalled.addListener(async function () {
-    console.log(await settings.cacheUrlPaths());
-    //   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    //     chrome.declarativeContent.onPageChanged.addRules([{
-    //       conditions: [new chrome.declarativeContent.PageStateMatcher({pageUrl: {hostEquals: 'developer.chrome.com'}})],
-    //       actions: [new chrome.declarativeContent.ShowPageAction()]
-    //     }]);
-    //   });
+    const urlPaths = await settings.cacheUrlPaths();
+    setPageRules(urlPaths);
 });
 
 chrome.storage.onChanged.addListener(async (changes, namespace) => {
     if (namespace === 'local' && changes.urlPaths) {
-        if (changes.urlPaths.newValue) {
-            // TODO enable page action
-        }
-        else chrome.declarativeContent.onPageChanged.removeRules();
+        setPageRules(changes.urlPaths.newValue);
     }
 });
