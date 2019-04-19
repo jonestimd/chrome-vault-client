c30cc13b1042b0c717a9aee37b5ccd5e406274ff
Tim Jones <jonestimd@gmail.com>
2019-04-18 20:13:45 -0500

    Moved ListItem into List

--- src/lib/components/ListItem.js
+++
@@ -1,6 +0,0 @@
-export function newListItem(text) {
-    const li = document.createElement('li');
-    li.className = 'mdc-list-item';
-    li.innerHTML = `<span class="mdc-list-item__text">${text}</span>`;
-    return li;
-}
