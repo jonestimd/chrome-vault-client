3674b3df63f5146e1984c970b2b7f31930a844cf
Tim Jones <jonestimd@gmail.com>
2019-05-11 15:41:55 -0500

    Fix contentScript to work with pages containing React components

--- src/lib/contentScript.js
+++ src/lib/contentScript.js
@@ -6,11 +6,19 @@ chrome.runtime.onMessage.addListener(function(message) {
     if (message) {
         if (message.username) {
             const userInput = document.querySelector('input[id*="user" i]');
-            if (userInput) userInput.value = message.username;
+            if (userInput) {
+                userInput.setAttribute('value', message.username);
+                userInput.dispatchEvent(new Event("change", {bubbles: true}));
+                if (userInput.value !== message.username) userInput.value = message.username;
+            }
         }
         if (message.password) {
             const passwordInput = document.querySelector('input[type="password"]');
-            if (passwordInput) passwordInput.value = message.password;
+            if (passwordInput) {
+                passwordInput.setAttribute('value', message.password);
+                passwordInput.dispatchEvent(new Event("change", {bubbles: true}));
+                if (passwordInput.value !== message.password) passwordInput.value = message.password;
+            }
         }
     }
 });
--- src/test/contentScript.test.js
+++ src/test/contentScript.test.js
@@ -6,6 +6,19 @@ import {JSDOM} from 'jsdom';
 const windowUrl = 'https://some.site';
 const username = 'site user';
 const password = 'passw0rd';
+const event = {type: 'change'};
+
+function stubProperty(obj, name, value) {
+    const def = {
+        get: sinon.stub().returns(value),
+        set: sinon.stub()
+    };
+    Object.defineProperty(obj, name, def);
+    return def;
+}
+function stubEach(obj, ...props) {
+    return props.reduce((stubs, prop) => Object.assign(stubs, {[prop]: sinon.stub(obj, prop)}), {});
+}
 
 module.exports = {
     'contentScript': {
@@ -45,28 +58,74 @@ module.exports = {
             expect(chrome.runtime.sendMessage).to.be.calledOnce
                 .calledWithExactly({username: false, password: true, url: windowUrl});
         },
-        'message listener': {
+        '@test message listener': {
+            beforeEach() {
+                global.Event = sinon.stub().returns(event);
+            },
+            afterEach() {
+                delete global.Event;
+            },
             'does nothing if message not provided': () => {
                 global.document = new JSDOM('<html></html>').window.document;
                 require('../lib/contentScript');
 
                 chrome.runtime.onMessage.addListener.args[0][0]();
             },
-            'populates username field': () => {
+            'populates username field using setAttribute': () => {
                 global.document = new JSDOM('<html><input type="text" id="username"/></html>').window.document;
+                const input = document.querySelector('input');
+                const value = stubProperty(input, 'value', username);
+                const {dispatchEvent, setAttribute} = stubEach(input, 'dispatchEvent', 'setAttribute');
                 require('../lib/contentScript');
-    
+
                 chrome.runtime.onMessage.addListener.args[0][0]({username});
 
-                expect(document.querySelector('input').value).to.equal(username);
+                expect(setAttribute).to.be.calledOnce.calledWithExactly('value', username);
+                expect(Event).to.be.calledOnce.calledWithExactly("change", {bubbles: true});
+                expect(dispatchEvent).to.be.calledOnce.calledWithExactly(event);
+                expect(value.set).to.not.be.called;
             },
-            'populates password field': () => {
-                global.document = new JSDOM('<html><input type="password"/></html>').window.document;
+            'populates username field using value when setAttribute fails': () => {
+                global.document = new JSDOM('<html><input type="text" id="username"/></html>').window.document;
+                const input = document.querySelector('input');
+                const value = stubProperty(input, 'value');
+                const {dispatchEvent, setAttribute} = stubEach(input, 'dispatchEvent', 'setAttribute');
                 require('../lib/contentScript');
-    
+
+                chrome.runtime.onMessage.addListener.args[0][0]({username});
+
+                expect(setAttribute).to.be.calledOnce.calledWithExactly('value', username);
+                expect(Event).to.be.calledOnce.calledWithExactly("change", {bubbles: true});
+                expect(dispatchEvent).to.be.calledOnce.calledWithExactly(event);
+                expect(value.set).to.be.calledOnce.calledWithExactly(username);
+            },
+            'populates password field using setAttribute': () => {
+                global.document = new JSDOM('<html><input type="password"/></html>').window.document;
+                const input = document.querySelector('input');
+                const value = stubProperty(input, 'value', password);
+                const {dispatchEvent, setAttribute} = stubEach(input, 'dispatchEvent', 'setAttribute');
+                require('../lib/contentScript');
+
                 chrome.runtime.onMessage.addListener.args[0][0]({password});
 
-                expect(document.querySelector('input').value).to.equal(password);
+                expect(setAttribute).to.be.calledOnce.calledWithExactly('value', password);
+                expect(Event).to.be.calledOnce.calledWithExactly("change", {bubbles: true});
+                expect(dispatchEvent).to.be.calledOnce.calledWithExactly(event);
+                expect(value.set).to.not.be.called;
+            },
+            'populates password field using value when setAttribute fails': () => {
+                global.document = new JSDOM('<html><input type="password"/></html>').window.document;
+                const input = document.querySelector('input');
+                const value = stubProperty(input, 'value');
+                const {dispatchEvent, setAttribute} = stubEach(input, 'dispatchEvent', 'setAttribute');
+                require('../lib/contentScript');
+
+                chrome.runtime.onMessage.addListener.args[0][0]({password});
+
+                expect(setAttribute).to.be.calledOnce.calledWithExactly('value', password);
+                expect(Event).to.be.calledOnce.calledWithExactly("change", {bubbles: true});
+                expect(dispatchEvent).to.be.calledOnce.calledWithExactly(event);
+                expect(value.set).to.be.calledOnce.calledWithExactly(password);
             }
         }
     }
